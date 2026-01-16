4183607df7e49d379828e1eddffed86280ea1cbf
Kassem Abdallah <kas.freedom@gmail.com>
2026-01-16 20:15:47 +0100

    Update athyr to v0.1.2

--- Formula/athyr.rb
+++ Formula/athyr.rb
@@ -1,31 +1,30 @@
 # Athyr Homebrew Formula
-# Auto-updated on release
 
 class Athyr < Formula
   desc "AI agent orchestration platform"
   homepage "https://athyr.tech"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.1/athyr_0.1.1_darwin_arm64.tar.gz"
-      sha256 "a8bd69ddd0c41986f730196cd2fc9b5c58b9928b08d58374d99b1f13d69372fe"
+      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.2/athyr_0.1.2_darwin_arm64.tar.gz"
+      sha256 "0efb8371d13bf0eda1fed779c3e7bcd5124eda432f185aef71bb180e6f13d247"
     end
     on_intel do
-      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.1/athyr_0.1.1_darwin_amd64.tar.gz"
-      sha256 "54c773fb1da96489b95fe0fe340e60c52e158bcd483e0d37e5eebe6a65b909c1"
+      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.2/athyr_0.1.2_darwin_amd64.tar.gz"
+      sha256 "f5708129c74dc7d40f7327b427658005e5aadfde135b8aa49a0f8f1bfb61adb6"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.1/athyr_0.1.1_linux_arm64.tar.gz"
-      sha256 "12b8ad7a24c69aa0c3e5ed495c4f3ac3e3a87add52c582d85b5fc04be678663e"
+      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.2/athyr_0.1.2_linux_arm64.tar.gz"
+      sha256 "39ed4de70d5fbc213ab6295a3aa897aa7bb7f632b516a221c61eecb55eacca7f"
     end
     on_intel do
-      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.1/athyr_0.1.1_linux_amd64.tar.gz"
-      sha256 "8adab783a3f47b1a82126f246908b44611e2ec9f95734cd8dcdcebacd01e5516"
+      url "https://github.com/athyr-tech/athyr-bin/releases/download/v0.1.2/athyr_0.1.2_linux_amd64.tar.gz"
+      sha256 "173a721279e0e5ec33b3d2aafc8bf9ec5f093c4cd7e24264b9bd074ca83808ab"
     end
   end
 
